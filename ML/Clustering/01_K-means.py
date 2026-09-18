aea2874edd51bdf66ae8734e0d1f3005e5f16f0c
terry <wlei091226@gmail.com>
2026-09-18 23:32:35 +0800

    260918

--- ML/Clustering/01_K-means.py
+++ ML/Clustering/01_K-means.py
@@ -1,6 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
+plt.rcParams["font.sans-serif"] = [
+    "Noto Sans CJK SC"
+]
 
+plt.rcParams["axes.unicode_minus"] = False
 
 def kmeans(X, K, max_iter=100, tol=1e-4):
 
@@ -89,4 +93,30 @@ plt.scatter(X[:, 0], X[:, 1], c=labels)
 
 plt.scatter(centroids[:, 0], centroids[:, 1], marker="X", s=200)
 
+plt.show()
+
+##使用API
+from sklearn.cluster import KMeans
+from sklearn.datasets import make_blobs
+
+
+
+# 使用 make_blobs 生成 3 个簇，每个簇 100 个点
+X, y_true = make_blobs(n_samples=300, centers=3, cluster_std=2)
+
+fig, ax = plt.subplots(2, figsize=(8, 8))
+ax[0].scatter(X[:, 0], X[:, 1], s=50, c="gray", label="原始数据")
+ax[0].set_title("原始数据")
+ax[0].legend()
+
+# 使用 K-Means 聚类
+kmeans = KMeans(n_clusters=3)
+kmeans.fit(X)
+y_kmeans = kmeans.predict(X) # 预测每个点的簇标签
+centers = kmeans.cluster_centers_ # 获取簇中心
+
+ax[1].scatter(X[:, 0], X[:, 1], s=50, c=y_kmeans)
+ax[1].scatter(centers[:, 0], centers[:, 1], s=200, c="red", marker="o", label="簇中心")
+ax[1].set_title("K-means 聚类结果 (K=3)")
+ax[1].legend()
 plt.show()
